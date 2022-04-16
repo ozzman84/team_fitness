4b4909f937fb61fb7f1ce3e95fbeaac9d1a0dde1
Ozzie <57572826+ozzman84@users.noreply.github.com>
2022-04-16 11:46:16 -0600

    Add view for a trainer to see it's athletes

---
+++ spec/features/trainer_athletes/index_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe "Trainer's Athletes Index" do
+  let!(:trainers) { create_list(:trainer, 2) }
+  let!(:athletes) {
+    create_list(:athlete, 3, previous_coaching: true, trainer_id: trainers.first.id)
+  }
+  let!(:other_athletes) { create(:athlete, trainer_id: trainers.last.id) }
+  before { visit "/trainers/#{trainers.first.id}/athletes" }
+
+  it 'has the correct path' do
+    expect(current_path).to eq("/trainers/#{trainers.first.id}/athletes")
+  end
+
+  it 'displays All first Trainers Athletes' do
+    trainers.first.athletes.each do |athlete|
+      expect(page).to have_content(athlete.name)
+    end
+  end
+
+  it 'does not display last trainers athletes' do
+    expect(page).to_not have_content(other_athletes.name)
+  end
+end
